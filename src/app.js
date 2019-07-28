5a497073d5aeaa1562ec02aa83db85107361e23f
Rayyan <5747484+rayyanQ@users.noreply.github.com>
2019-07-27 22:34:20 -0400

    adding jsx

---
+++ src/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <h1>Hello</h1>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("home"));
